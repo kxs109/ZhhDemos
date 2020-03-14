ee48da21e5b7cad52a4e6179d03ea342bc50dab0
zhuhuanhuan <huanhuan.zhu@ys100.com>
2020-03-14 10:59:48 +0800

    初始化

---
+++ app/src/main/kotlin/com/example/zhhdemos/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.example.zhhdemos
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity:AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onStart() {
+        super.onStart()
+    }
+}
