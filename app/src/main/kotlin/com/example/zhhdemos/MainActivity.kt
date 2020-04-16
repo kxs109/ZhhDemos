9dd545b49cffaadddd8dc72a6bc9ceb0c11435e7
zhuhuanhuan <huanhuan.zhu@ys100.com>
2020-04-16 21:35:44 +0800

    组件化搭建

--- app/src/main/kotlin/com/example/zhhdemos/MainActivity.kt
+++ app/src/main/kotlin/com/example/zhhdemos/MainActivity.kt
@@ -4,10 +4,15 @@ import com.alibaba.android.arouter.facade.annotation.Route
 import com.alibaba.android.arouter.launcher.ARouter
 import com.kxs109.commonlib.config.AppMain
 import com.kxs109.commonlib.config.KotlinLearnMain
-import com.kxs109.commonlib.config.base.BaseActivity
+import com.kxs109.mvvmbase.BaseActivity
 import kotlinx.android.synthetic.main.app_activity_main.*
 @Route(path = AppMain)
-class MainActivity:BaseActivity() {
+class MainActivity: BaseActivity() {
+    override fun initData() {
+    }
+
+    override fun getResLayout(): Int =R.layout.app_activity_main
+
     override fun initView() {
         kotlinLearnBtn.setOnClickListener {
             ARouter.getInstance().build(KotlinLearnMain) // 目标页面
@@ -16,7 +21,6 @@ class MainActivity:BaseActivity() {
         }
     }
 
-    override fun attachLayoutRes(): Int =R.layout.app_activity_main
 
 
 }
--- kotlinlearn/src/main/java/com/kxs109/kotlinlearn/MainActivity.kt
+++ kotlinlearn/src/main/java/com/kxs109/kotlinlearn/MainActivity.kt
@@ -2,15 +2,19 @@ package com.kxs109.kotlinlearn
 
 import com.alibaba.android.arouter.facade.annotation.Route
 import com.kxs109.commonlib.config.KotlinLearnMain
-import com.kxs109.commonlib.config.base.BaseActivity
 import com.kxs109.commonlib.config.utils.ext.logD
+import com.kxs109.mvvmbase.BaseActivity
 
 
 @Route(path = KotlinLearnMain)
 class MainActivity : BaseActivity() {
+    override fun initData() {
+    }
+
+    override fun getResLayout(): Int =R.layout.kotlin_learn_activity_main
+
     override fun initView() {
         intent.getStringExtra("key1").logD()
     }
 
-    override fun attachLayoutRes(): Int = R.layout.kotlin_learn_activity_main
 }
