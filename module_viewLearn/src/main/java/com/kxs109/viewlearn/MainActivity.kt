6ed4bcba427a62d8a39fca048baa959b100695da
zhuhuanhuan <huanhuan.zhu@ys100.com>
2020-08-25 23:20:28 +0800

    fix gradle

--- module_viewLearn/src/main/java/com/kxs109/viewlearn/MainActivity.kt
+++ module_viewLearn/src/main/java/com/kxs109/viewlearn/MainActivity.kt
@@ -8,5 +8,9 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.view_learn_activity_main)
+
+        "sddd".sumBy {
+            it.toInt()
+        }
     }
 }
